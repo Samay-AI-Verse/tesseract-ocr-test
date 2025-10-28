9f7120cdd72f6417664f6f17f4c8bb0370a6ecd7
Samay <samaypowade9@gmail.com>
2025-10-28 18:22:23 +0530

    my changes

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Update and install system dependencies
+# Install system dependencies
 apt-get update
 apt-get install -y tesseract-ocr poppler-utils
 
-# Optional: add language packs for Hindi, Marathi, Malayalam
+# Add Hindi, Marathi, Malayalam language packs
 apt-get install -y tesseract-ocr-hin tesseract-ocr-mar tesseract-ocr-mal
 
-# Install Python dependencies
+# Install Python packages
 pip install --upgrade pip
 pip install -r requirements.txt
